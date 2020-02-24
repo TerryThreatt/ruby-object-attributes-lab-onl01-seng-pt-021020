c1f67d4b26e50d893e84190783b7f029cb0c3fa7
Terry Threatt <terry.threatt@gmail.com>
2020-02-24 18:47:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
 class Dog 
   def name=(name) 
-    @this_dogs_name = name
+    @name = name
   end 
   
   def name 
-    @this_dogs_name
+    @name
   end 
   
-  def breed=(dog_breed)
-    @this_dogs_breed = dog_breed
+  def breed=(breed)
+    @breed = breed
   end 
   
   def breed 
-    @this_dogs_breed
+    @breed
   end 
 end 
